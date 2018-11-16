22afb4c72e41b8dcca4e4304f827f963aab63928
Dauerdaddlah <hertel-dennis@web.de>
2018-11-16 10:17:27 +0100

    :art: replace JUnit4 code with its JUnit5 pendant

--- src/test/kotlin/org/oso/core/ApplicationTest.kt
+++ src/test/kotlin/org/oso/core/ApplicationTest.kt
@@ -1,14 +1,15 @@
 package org.oso.core
 
-import org.junit.Test
-import org.junit.runner.RunWith
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.oso.Application
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest(classes=[Application::class])
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @ActiveProfiles("test")
 class ApplicationTest {
     /**
--- src/test/kotlin/org/oso/core/services/impl/OpenStreetMapsServiceTest.kt
+++ src/test/kotlin/org/oso/core/services/impl/OpenStreetMapsServiceTest.kt
@@ -1,19 +1,19 @@
 package org.oso.core.services.impl
 
 import org.junit.Assert
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.oso.Application
 import org.oso.core.entities.Coordinates
 import org.oso.core.services.external.impl.OpenStreetMapsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.math.BigDecimal
 
 @SpringBootTest(classes=[Application::class])
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @ActiveProfiles("test")
 class OpenStreetMapsServiceTest {
 
