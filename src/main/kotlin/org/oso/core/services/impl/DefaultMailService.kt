6893faf67249ef7c65f8095d1274a37e0b359eff
Valefant <valentino.bernardo_ciddio@smail.th-koeln.de>
2018-12-28 22:20:14 +0100

    :art: Log mail sending and small refactoring

--- src/main/kotlin/org/oso/core/services/impl/DefaultMailService.kt
+++ src/main/kotlin/org/oso/core/services/impl/DefaultMailService.kt
@@ -1,8 +1,11 @@
 package org.oso.core.services.impl
 
 import org.oso.core.services.MailService
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.MailException
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.JavaMailSenderImpl
@@ -11,30 +14,47 @@ import org.springframework.stereotype.Service
 @Service
 class DefaultMailService
     /**
-    * @constructor
+    * @constructor      Creates a default mail service.
     * @param mailSender Spring creates this bean automatically when it is not defined.
      *                  It takes the mail properties as program argument or from the application.yml.
+     *                  Therefore it is annotated with @Suppress.
     */
-    @Autowired constructor(private val mailSender: JavaMailSender) : MailService {
-    private val FROM = "kontakt@ososystem.de"
+    @Autowired constructor(
+        @Value("\${email.from}")
+        private val FROM: String,
+        @Suppress
+        private val mailSender: JavaMailSender
+    ) : MailService {
 
     override fun send(participants: List<String>, subject: String, text: String) {
-        mailSender as JavaMailSenderImpl
-        mailSender.javaMailProperties.put("mail.smtp.ssl.enable", "true")
+        require(FROM.isNotEmpty()) { "The email.from key should be set for an outgoing email." }
+
+        if (mailSender !is JavaMailSenderImpl) {
+            return
+        }
+
+        mailSender.javaMailProperties["mail.smtp.ssl.enable"] = "true"
 
         participants
             .map { participant ->
-                val message = SimpleMailMessage()
-                with(message) {
+                SimpleMailMessage().apply {
                     setFrom(FROM)
                     setTo(participant)
                     setSubject(subject)
                     setText(text)
                 }
-                message
             }
             .forEach { mail ->
-                mailSender.send(mail)
+                try {
+                    mailSender.send(mail)
+                    LOGGER.info("Mail was sent to {}.", mail)
+                } catch (e: MailException) {
+                    LOGGER.error("{}", e.localizedMessage, e)
+                }
             }
+    }
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(DefaultHelpProviderService::class.java)
     }
 }
