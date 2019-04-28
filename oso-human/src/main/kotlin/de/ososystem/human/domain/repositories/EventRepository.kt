b25d88ae4a0c457b5101daedc11205cfcde33c76
Dauerdaddlah <hertel-dennis@web.de>
2019-04-28 17:21:38 +0200

    remove unneeded method

--- oso-human/src/main/kotlin/de/ososystem/human/domain/repositories/EventRepository.kt
+++ oso-human/src/main/kotlin/de/ososystem/human/domain/repositories/EventRepository.kt
@@ -1,11 +1,9 @@
 package de.ososystem.human.domain.repositories
 
 import de.ososystem.human.domain.events.HumanEvent
-import de.ososystem.human.domain.events.Id
 
 interface EventRepository {
     fun saveEvent(event: HumanEvent): HumanEvent
     fun deleteEvent(event: HumanEvent)
-    fun deleteHumanById(id: Id)
     fun findEventWithHighestId(): HumanEvent?
 }
