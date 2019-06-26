31a6e1f416a60898d134b2ac46151c39974486b6
VincentDiedrich <vincentdiedrich@protonmail.com>
2019-06-26 19:50:38 +0200

    [generate api-doc] Outsourced Travis backend generated documentation

---
+++ push-api-documentation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USER=$1
+EMAIL=$2
+DOC_PATH=$3
+FILE=$4
+REPO=$5
+GH_REPO=$6
+MESSAGE=$7
+TOKEN=$8
+
+if [[ $MESSAGE == *"[generate api-doc]"* ]]; then
+    git clone git://${GH_REPO}
+    mv -f $DOC_PATH/$FILE $REPO
+    cd $REPO
+    git remote
+    git config user.email $EMAIL
+    git config user.name $USER
+    git config user.email
+    git config user.name
+    git add $FILE
+    git commit -m "$MESSAGE"
+    git push "https://$TOKEN@$GH_REPO" master
+fi
