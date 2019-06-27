fe9f40c49aa39e5adc67524cde82bc2adc448034
VincentDiedrich <vincentdiedrich@protonmail.com>
2019-06-27 20:31:16 +0200

    [generate api-doc] Fixed detection issue

--- push-api-documentation.sh
+++ push-api-documentation.sh
@@ -9,7 +9,8 @@ GH_REPO=$6
 MESSAGE=$7
 TOKEN=$8
 
-if [[ $MESSAGE == *"[generate api-doc]"* ]]; then
+if [[ $MESSAGE =~ .*[generate api-doc].* ]];
+then
     git clone git://$GH_REPO
     mv -f $DOC_PATH/$FILE $REPO
     cd $REPO
